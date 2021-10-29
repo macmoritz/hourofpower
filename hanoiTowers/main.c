df1d89f1a32147f48638d7b8aa953d55b9483df5
macmoritz <49832924+macmoritz@users.noreply.github.com>
2021-10-29 14:11:37 +0200

    finish hanoi towers

--- hanoiTowers/main.c
+++ hanoiTowers/main.c
@@ -1,23 +1,40 @@
+#include <stdbool.h>
 #include <stdio.h>
 
-#define slice_count 3
+#define slice_count 15
 
-int doMoves(n, start, mid, end) {
-    if (n == slice_count) {
-        return 0;
-    }
 
-    if (n == 1) {
-        if (n % 2) {
-            doMoves(n + 1, start, end, mid);
-        } else {
-            doMoves(n + 1, start, mid, end);
+void moveDisk(int disk, int from[], int to[]) {
+    bool set = false;
+    for(int i = slice_count - 1; i >= 0; i--) {
+        if(from[i] == disk) {
+            from[i] = 0;
+        }
+        if(to[i] == 0 && !set) {
+            to[i] = disk;
+            set = true;
         }
     }
-
 }
 
-int main() {
+void printTowers(int a[], int b[], int c[]) {
+    for(int i = 0; i < slice_count; i++) {
+        printf("\t%d\t%d\t%d\n", a[i], b[i], c[i]);
+    }
+    printf("\t-----------------\n\ta\tb\tc\n\n");
+}
+
+void moveTower(int n, int a[], int b[], int c[]) {
+    if (n == 1) {
+        moveDisk(1, a, b);
+    } else {
+        moveTower(n-1, a, c, b);
+        moveDisk(n, a, b);
+        moveTower(n-1, c, b, a);
+    }
+}
+
+int main(int argc, char **argv) {
     int a[slice_count] = {};
     int b[slice_count] = {};
     int c[slice_count] = {};
@@ -26,7 +43,9 @@ int main() {
         a[i] = i + 1;
     }
 
-    doMoves(1, a, b, c);
-
+    printTowers(a, b, c);
+    moveTower(slice_count, a, c, b);
+    printf("\n");
+    printTowers(a, b, c);
     return 0;
 }
