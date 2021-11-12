8fe8dfefb95df486acc4c827f0903bb6df5972db
moritz <m.tratar@senbax.computer>
2021-11-12 22:38:25 +0100

    more clean print

--- resistors/script.py
+++ resistors/script.py
@@ -4,5 +4,5 @@ with open('Widerstand.txt') as file:
     for line in file:
         line = line.strip().split('=')
         formula = re.sub(r'<([0-9]+)/([0-9]+)>', r'1/(1/\1+1/\2)', line[1]).replace('<', '(').replace('>', ')').replace('-', '+')
-        print(f'{line[0]}={eval(formula)} ≈ {int(round(eval(formula), -1))}')
+        print(f'{line[0]}\t=\t{eval(formula)}\t≈\t{int(round(eval(formula), -1))}')
 
