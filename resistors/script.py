191619ea4cb50cf12bf973e34ca18c2717678e94
moritz <m.tratar@senbax.computer>
2021-11-12 22:37:06 +0100

    i <3 regex (strange results)

--- resistors/script.py
+++ resistors/script.py
@@ -1,5 +1,8 @@
+import re
+
 with open('Widerstand.txt') as file:
     for line in file:
         line = line.strip().split('=')
-        print(f'{line[0]}={eval(line[1].replace("<", "(").replace(">", ")").replace("-", "+"))}')
-file.close()
+        formula = re.sub(r'<([0-9]+)/([0-9]+)>', r'1/(1/\1+1/\2)', line[1]).replace('<', '(').replace('>', ')').replace('-', '+')
+        print(f'{line[0]}={eval(formula)} ≈ {int(round(eval(formula), -1))}')
+
